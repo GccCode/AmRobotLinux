796b0d0ea02b93050091b718abbb91f832f3fd13
bill <gcccode@gmail.com>
2018-09-21 20:45:00 +0800

    fix...

--- amspider.py
+++ amspider.py
@@ -464,7 +464,6 @@ if __name__ == "__main__":
     chrome_options.add_experimental_option("prefs", prefs)
     chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
     chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
-    chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
     chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.set_page_load_timeout(60)
