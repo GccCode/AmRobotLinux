b219f3eae801c842b62de652722b9df278b7f102
bill <gcccode@gmail.com>
2018-09-21 15:44:42 +0800

    fix...

--- amspider.py
+++ amspider.py
@@ -371,7 +371,7 @@ def test_get_inventory_us():
 
 def test_get_inventory_jp(): # driver, asin):
     chrome_options = webdriver.ChromeOptions()
-    prefs = {"profile.managed_default_content_settings.images": 2}
+    prefs = {"profile.default_content_setting_values.images": 2}
     chrome_options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome()
     driver.set_page_load_timeout(60)
