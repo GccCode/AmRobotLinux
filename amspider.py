5d40b5e9a9aecb68164a91efdbb84c630ae29d56
bill <gcccode@gmail.com>
2018-09-25 22:04:46 +0800

    fix...

--- amspider.py
+++ amspider.py
@@ -277,8 +277,8 @@ class AmazonSpider():
                             tmp_symbol = NON_CRITICAL_HAS_REVIEW_FBM_PRICE_PREFIX + str(i + 1) + NON_CRITICAL_HAS_REVIEW_FBM_PRICE_POSTFIX
                         if amazonpage.is_element_exsist(*(By.XPATH, tmp_symbol)):
                             element = driver.find_element_by_xpath(tmp_symbol)
+                            print("Price is : " + element.text.strip('￥ ').replace(',', ''), flush=True)
                             asin_info_data['price'] = int(element.text.strip('￥ ').replace(',', ''))
-                            # print("Price is : " + element.text.strip('￥ ').replace(',', ''), flush=True)
                     else:
                         tmp_symbol = NON_CRITICAL_FBA_PREFIX + str(i + 1) + NON_CRITICAL_FBA_POSTFIX
                         if amazonpage.is_element_exsist(*(By.XPATH, tmp_symbol)):
@@ -293,8 +293,8 @@ class AmazonSpider():
                                 i + 1) + NON_CRITICAL_NO_REVIEW_FBM_PRICE_POSTFIX
                         if amazonpage.is_element_exsist(*(By.XPATH, tmp_symbol)):
                             element = driver.find_element_by_xpath(tmp_symbol)
+                            print("Price is : " + element.text.strip('￥ ').replace(',', ''), flush=True)
                             asin_info_data['price'] = int(element.text.strip('￥ ').replace(',', ''))
-                            # print("Price is : " + element.text.strip('￥ ').replace(',', ''), flush=True)
 
                     tmp_symbol = NON_CRITICAL_IMGSRC_PREFIX + str(i + 1) + NON_CRITICAL_IMGSRC_POSTFIX
                     if amazonpage.is_element_exsist(*(By.XPATH, tmp_symbol)):
