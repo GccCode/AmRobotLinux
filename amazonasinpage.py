d5cd4f83606f3ea0ddc1fa10ecbe916f8ea4100a
bill <gcccode@gmail.com>
2018-09-26 10:38:08 +0800

    fix...

--- amazonasinpage.py
+++ amazonasinpage.py
@@ -41,26 +41,31 @@ class AmazonAsinPage(AmazonPage):
                 try:
                     element = self.driver.find_element_by_id(OPTIONS_JP_PREFIX + str(total))
                     option_array.append(element)
-                    print(element.get_attribute('class'))
-                    print(element.get_attribute('value'))
                 except NoSuchElementException as msg:
-                    print("yyyy", flush=True)
                     break
-            print(total)
 
             for index in range(0, total):
                 value = option_array[index].get_attribute('value').split(',')
-                print(asin)
-                print(value[1])
                 if asin == value[1]:
-                    print(option_array[index].get_attribute('class'))
+                    # print(option_array[index].get_attribute('class'))
                     if option_array[index].get_attribute('class') == 'dropdownAvailable':
-                        print("sjdflsdjlk")
-            input("xxxx")
+                        print("return the original aisn", flush=True)
+                        url = 'https://www.amazon.co.jp/dp/' + asin + '?th=1&psc=1'
+                        self.driver.get(url)
+
+            for index in range(0, total):
+                value = option_array[index].get_attribute('value').split(',')
+                # print(asin)
+                # print(value[1])
+                if option_array[index].get_attribute('class') == 'dropdownAvailable':
+                    print("find another asin to replace..", flush=True)
+                    url = 'https://www.amazon.co.jp/dp/' + value[1] + '?th=1&psc=1'
+                    self.driver.get(url)
             # self.select(index, *self.locator.SELECT_SIZE_JP)
             self.random_sleep(begin, end)
         else:
             print("Size Selection is not here", flush=True)
+
 
     def ask_qa(self, content, begin, end):
         country = self.cf.get("account", "country")
