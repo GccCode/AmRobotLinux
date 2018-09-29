d9b8f884f999d754311025b545636b439a82be74
gcccode <gcccode@gmail.com>
2018-09-29 16:45:43 +0800

    fix...

--- amazonsql.py
+++ amazonsql.py
@@ -157,7 +157,6 @@ class AmazonSql():
         # 'UPDATE students SET age = %s WHERE name = %s'
         status = True
         sql = 'UPDATE {table} SET {key} = {value} WHERE {condition}'.format(table=table, key=key, value=value, condition=condition)
-        print(sql)
         try:
             cursor = db.cursor()
             cursor.execute(sql)
