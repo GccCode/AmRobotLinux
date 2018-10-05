bb6080d9d02d47e8457e8937bf5a137c31672271
bill <gcccode@gmail.com>
2018-10-05 13:29:48 +0800

    fix...

--- amazonsigninpage.py
+++ amazonsigninpage.py
@@ -22,32 +22,74 @@ class AmazonSignInPage(AmazonPage):
         print(("** 开始保存cookies。。。"), flush=True)
         self.save_cookies()
 
+    # def fill_in_form(self):
+    #     emailname = self.cf.get("account", "email")
+    #     password = self.cf.get("account", "password")
+    #     try:
+    #         self.click(*self.locator.ACCOUNTSWITCHER)
+    #         self.random_sleep(1000, 2000)
+    #     except NoSuchElementException as msg:
+    #         self.click(*self.locator.EMAILNAME)
+    #         self.random_sleep(1000, 2000)
+    #         self.input(emailname, *self.locator.EMAILNAME)
+    #         self.random_sleep(1000, 2000)
+    #         self.click(*self.locator.CONTINUE)
+    #         self.random_sleep(1000, 2000)
+    #         self.click(*self.locator.PASSWORD)
+    #         self.random_sleep(1000, 2000)
+    #         self.input(password, *self.locator.PASSWORD)
+    #         self.random_sleep(1000, 2000)
+    #         self.click(*self.locator.SIGNINSUBMIT)
+    #     else:
+    #         self.click(*self.locator.PASSWORD)
+    #         self.random_sleep(1000, 2000)
+    #         self.input(password, *self.locator.PASSWORD)
+    #         self.random_sleep(1000, 2000)
+    #         self.click(*self.locator.SIGNINSUBMIT)
+
     def fill_in_form(self):
         emailname = self.cf.get("account", "email")
         password = self.cf.get("account", "password")
-        try:
+        if self.is_element_exsist(*self.locator.ACCOUNTSWITCHER):
             self.click(*self.locator.ACCOUNTSWITCHER)
             self.random_sleep(1000, 2000)
-        except NoSuchElementException as msg:
-            if self.is_element_exsist(*self.locator.EMAILNAME):
-                print("xxxxxxxxxx", flush=True)
+            if self.is_element_exsist(*self.locator.PASSWORD):
+                self.click(*self.locator.PASSWORD)
+                self.random_sleep(1000, 2000)
+                self.input(password, *self.locator.PASSWORD)
+                self.random_sleep(1000, 2000)
+                if self.is_element_exsist(*self.locator.SIGNINSUBMIT):
+                    self.click(*self.locator.SIGNINSUBMIT)
+                else:
+                    print("找不到登录提交按钮...", flush=True)
+                    exit(-1)
             else:
-                print("yyy",flush=True)
+                print("找不到密码输入栏...", flush=True)
+                exit(-1)
+        elif self.is_element_exsist(*self.locator.EMAILNAME):
             self.click(*self.locator.EMAILNAME)
             self.random_sleep(1000, 2000)
             self.input(emailname, *self.locator.EMAILNAME)
             self.random_sleep(1000, 2000)
-            self.click(*self.locator.CONTINUE)
-            self.random_sleep(1000, 2000)
-            self.click(*self.locator.PASSWORD)
-            self.random_sleep(1000, 2000)
-            self.input(password, *self.locator.PASSWORD)
-            self.random_sleep(1000, 2000)
-            self.click(*self.locator.SIGNINSUBMIT)
+            if self.is_element_exsist(*self.locator.CONTINUE):
+                self.click(*self.locator.CONTINUE)
+                self.random_sleep(1000, 2000)
+                if self.is_element_exsist(*self.locator.PASSWORD):
+                    self.click(*self.locator.PASSWORD)
+                    self.random_sleep(1000, 2000)
+                    self.input(password, *self.locator.PASSWORD)
+                    self.random_sleep(1000, 2000)
+                    if self.is_element_exsist(*self.locator.SIGNINSUBMIT):
+                        self.click(*self.locator.SIGNINSUBMIT)
+                    else:
+                        print("找不到登录提交按钮...", flush=True)
+                        exit(-1)
+                else:
+                    print("找不到密码输入栏...", flush=True)
+                    exit(-1)
+            else:
+                print("找不到继续按钮跳到输入密码页面。。。", flush=True)
+                exit(-1)
         else:
-            self.click(*self.locator.PASSWORD)
-            self.random_sleep(1000, 2000)
-            self.input(password, *self.locator.PASSWORD)
-            self.random_sleep(1000, 2000)
-            self.click(*self.locator.SIGNINSUBMIT)
-
+            print("填写登录信息出错！", flush=True)
+            exit(-1)
