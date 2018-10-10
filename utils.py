1c29b44931efb520767a38b15b07e330091a85fd
gcccode <gcccode@gmail.com>
2018-10-10 17:01:59 +0800

    fix...

--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ import traceback
 #8) WinXP+ie7：
 #9) WinXP+ie6：
 useragentlist = [
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
+    # 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
     # 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)',
