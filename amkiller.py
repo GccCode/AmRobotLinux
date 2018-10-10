85a170b7fcee7962f2366f63e25c82d0d9ebaa29
gcccode <gcccode@gmail.com>
2018-10-10 15:51:01 +0800

    fix...

--- amkiller.py
+++ amkiller.py
@@ -40,13 +40,17 @@ if __name__ == "__main__":
             continue
         keyword = admin.get_random_task()
         whiteasin = admin.get_whiteasin(keyword)
+        if whiteasin != False:
+            print(whiteasin, flush=True)
         blackasin = admin.get_blackasin(keyword)
+        if blackasin != False:
+            print(blackasin, flush=True)
         driver = utils.customized_broswer_with_luminati(ips_array)
         t1 = time.time()
         amazonpage = AmazonPage(driver)
         try:
             tmp = random.randint(1, 100)
-            if tmp < 50:
+            if tmp < 30:
                 ## registeration
                 print(("* Registeration..."), flush=True)
                 amazonpage.enter_amazon_page(3000, 5000)
