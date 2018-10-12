1a20667e773272e83c6b81f7911ad98ab6ed20a7
gcccode <gcccode@gmail.com>
2018-10-12 19:42:30 +0800

    fix...

--- amspider.py
+++ amspider.py
@@ -891,9 +891,6 @@ class AmazonSpider():
             else:
                 data['shipping'] = 'FBM'
 
-            print(data['shipping'], flush=True)
-            input('xxxx')
-
             if amazonasinpage.is_element_exsist(*QA_COUNT):
                 element = driver.find_element(*QA_COUNT)
                 data['qa'] = int(getqa_us(element.text))
@@ -1302,9 +1299,6 @@ def amspider_test(node, node_name, type):
 
 if __name__ == "__main__":
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-
-    amspider_test('13591416011', 'XXXX', 'BS')
-
     node_file = sys.argv[1]
     if node_file != '0':
         type = sys.argv[2]
