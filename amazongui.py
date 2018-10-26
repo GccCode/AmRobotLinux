77e4f03e14b4749e082225cea5e922e4fec355f9
gcccode <gcccode@gmail.com>
2018-10-26 16:42:09 +0800

    fix...

--- amazongui.py
+++ amazongui.py
@@ -89,7 +89,7 @@ class AmazonGUI():
                 "库存",
                 days_data_array,
                 inventory_data_array,
-                yaxis_max=250,
+                yaxis_max=1050,
                 legend_pos="85%",
                 legend_orient="vertical",
                 legend_top="45%",
@@ -98,7 +98,7 @@ class AmazonGUI():
             line.add("销量", days_data_array, sale_data_array, mark_point=["max"], mark_line=["average"])
             overlap = Overlap(width=1200, height=600)
             overlap.add(bar)
-            overlap.add(line, is_add_yaxis=True, yaxis_index=1)
+            overlap.add(line, is_add_yaxis=True, yaxis_index=200)
 
             grid.add(overlap, grid_right="20%")
             filename = '../html_page/daily_sale/' + asin + '.html'
