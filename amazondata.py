8fb76a47e03d95e39b1945b0ad11d263b9d54d72
gcccode <gcccode@gmail.com>
2018-11-03 23:02:00 +0800

    fix...

--- amazondata.py
+++ amazondata.py
@@ -273,84 +273,44 @@ class AmazonData():
         else:
             return False
 
+    # def get_column_avg(self, table, column):
+    #     status = -999
+    #     sql = 'select avg(' + column + ') from ' + table
+    #     cursor = self.amsql.query(self.db, sql)
+    #     if cursor != False:
+    #         avg_rows = cursor.fetchall()
+    #         status = avg_rows[0][0]
+    #         if status < 0:
+    #             status = 0
+    #
+    #     return status
+
     def get_column_avg(self, table, column):
         status = -999
-        sql = 'select avg(' + column + ') from ' + table
+        sql = 'select ' + column + ' from ' + table
         cursor = self.amsql.query(self.db, sql)
         if cursor != False:
             avg_rows = cursor.fetchall()
-            status = avg_rows[0][0]
-            if status < 0:
-                status = 0
+            total = 0
+            if len(avg_rows) > 1:
+                for sale in avg_rows:
+                    total += sale[0]
 
+                status = int(total / (len(avg_rows) - 1))
         return status
 
 
 if __name__ == "__main__":
     amazondata = AmazonData()
-    status = amazondata.create_database('amazondata')
+    status = amazondata.create_database('data_us')
     if status == False:
         print("create database fail...", flush=True)
-    else:
-        print("create database ok...", flush=True)
-    status = amazondata.connect_database('amazondata')
+        exit()
+    status = amazondata.connect_database('data_us')
     if status == False:
         print("connect to amazondata database fail..", flush=True)
-    else:
-        print("connect to default database ok..", flush=True)
-        amazondata.disconnect_database()
+        exit()
 
-    # status = amazondata.update_data('2285178051_bs_B07GP8QY8H', 'avg_sale', '30', 'asin=\'B07D8LJ9F2\'')
-    # if status == False:
-    #     print("xxx")
-    # else:
-    #     print("uuu")
-    # status = amazondata.create_database(amazondata.db, 'amazondata')
-    # if status == False:
-    #     print("create amazondata database fail..", flush=True)
-    # else:
-    #     print("create amazondata database ok..", flush=True)
-
-    # status = amazondata.create_asin_info_table('2285178051_bs_B07GP8QY8H')
-    # if status == False:
-    #     print("Create table fail", flush=True)
-    # else:
-    #     data = {"rank": 7, "asin": "B07D8LJ9F2", "node": "2285178051", "price": 740, "review": 2, "rate": 1.0, "qa": 1, "shipping": "FBM", "seller": 1, "avg_sale": 0, "limited": "no", "img_url": "4124d4tNQbL", "status": "ok"}
-    #     status = amazondata.insert_asin_info_data('2285178051_bs_B07GP8QY8H', data)
-    #     if status == False:
-    #         print("Data Insert fail", flush=True)
-    #     else:
-    #         print("Data Insert ok")
-    # status = amazondata.create_inventory_table('inventory_B07D8LJ9F2')
-    # if status == False:
-    #     print("fail")
-    # else:
-    #     print("ok")
-    # today = date.today()
-    # yesterday = today + timedelta(days=-1)
-    # today_data = {
-    #     'date': today,
-    #     'inventory': 9
-    # }
-    # status = amazondata.insert_inventory_data('inventory_B07D8LJ9F2', today_data)
-    # if status == False:
-    #     print("fail1")
-    # else:
-    #     print("ok1")
-    # yesterday_data = {
-    #     'date' : yesterday,
-    #     'inventory': 30
-    # }
-    # status = amazondata.insert_inventory_data('inventory_B07DPP7V2M', yesterday_data)
-    # if status == False:
-    #     print("fail2")
-    # else:
-    #     print("ok2")
-    #
-    # status = amazondata.get_yesterday_sale('inventory_B07DPP7V2M')
-    # if status == -1:
-    #     print("fail3")
-    # else:
-    #     print("today sale is " + str(status))
+    amazondata.get_column_avg('SALE_B075J76KFM', 'sale')
 
     amazondata.disconnect_database()
