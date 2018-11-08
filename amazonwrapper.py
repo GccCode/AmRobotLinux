6485b9e261f56127f9dd3692b7d1505550d3756c
gcccode <gcccode@gmail.com>
2018-11-08 11:17:22 +0800

    fix...

--- amazonwrapper.py
+++ amazonwrapper.py
@@ -6,6 +6,7 @@ import xlrd
 import random
 from time import strftime, localtime
 import calendar
+import datetime as dt
 from datetime import date
 from datetime import timedelta
 import amazonglobal
@@ -625,6 +626,26 @@ def delete_tables(amazondata, table_name_condition, condition):
     return False
 
 
+def get_table_existed_time(amazondata, table_name):
+    now_time = dt.datetime.now()
+    sql = 'select create_time from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA = ' + '\'' + amazondata.db_name + '\'' + 'and TABLE_NAME =' + '\'' + table_name + '\''
+    cursor = amazondata.query(sql)
+    if cursor is not False:
+        create_time_str = cursor.fetchone()[0].strftime('%Y-%m-%d %H:%M:%S')
+        create_time = dt.datetime.strptime(create_time_str, '%Y-%m-%d %H:%M:%S')
+        existed_time = now_time - create_time
+        return int(existed_time.days)
+    else:
+        print("Err: get create time for table " + table_name, flush=True)
+        return False
+
+def is_table_expired(amazondata, table_name, valid_days):
+    existed_days = get_table_existed_time(amazondata, table_name)
+    if existed_days >= valid_days:
+        return True
+
+    return False
+
 def delete_unused_tables(amazondata, table_name_condition, condition):
     if table_name_condition == False:
         sql = 'SHOW TABLES'
@@ -635,14 +656,22 @@ def delete_unused_tables(amazondata, table_name_condition, condition):
         result = cursor.fetchall()
         for index in range(len(result)):
             table_name = result[index][0]
-            data = get_all_data(amazondata, table_name, False, condition)
-            if data == False:
-                delete_sql = 'drop table ' + result[index][0]
-                print(delete_sql, flush=True)
-                cursor = amazondata.query(delete_sql)
-                # if cursor == False:
-                #     print("delete table in failure.. + " + db_name, flush=True)
+            if is_table_expired(amazondata, table_name, 4):
+                data = get_all_data(amazondata, table_name, False, condition)
+                if data == False:
+                    asin_array = get_all_data(amazondata, table_name, 'asin', False)
+                    if asin_array is not False:
+                        for i in range(len(asin_array)):
+                            sql = 'drop table ' + 'SALE_' + asin_array[i][0]
+                            print(sql, flush=True)
+                            # amazondata.query(sql)
+                            sql = 'drop table ' + 'INVENTORY_' + asin_array[i][0]
+                            print(sql, flush=True)
+                            # amazondata.query(sql)
 
+                    delete_sql = 'drop table ' + result[index][0]
+                    print(delete_sql, flush=True)
+                    # amazondata.query(delete_sql)
     else:
         print("get all table in failure.. + " + amazondata.db_name, flush=True)
 
@@ -889,9 +918,9 @@ if __name__ == "__main__":
     # add_new_column('data_us', '_BS', 'weight', 'weight FLOAT(10) NOT NULL default 0')
     # seller_name = get_one_data(amazonglobal.db_name_data_us, '9977442011_BS', 'asin=' + '\'' + 'B01EHSX28M' + '\'')
     # print(seller_name[16], flush=True)
-    # delete_unused_node_task(sqlmgr, 'avg_sale>5 and price>=15 and limited = \'no\'')
-    # delete_unused_tables(sqlmgr.ad_sale_data, '\'%\_BS\'', 'avg_sale>5 and price>10 and limited=\'no\'')
-    count_pending_asin(sqlmgr, 'BS')
+    delete_unused_tables(sqlmgr.ad_sale_data, '\'%\_BS\'', 'avg_sale>5 and price>=12 and limited=\'no\'')
+    # delete_unused_node_task(sqlmgr, 'avg_sale>5 and price>=12 and limited = \'no\'')
+    # count_pending_asin(sqlmgr, 'BS')
     # copy_table_data(sqlmgr.ad_sale_data, sqlmgr.ad_sale_task)
-
+    # get_table_existed_time(sqlmgr.ad_sale_data, 'GWA_BS')
     sqlmgr.stop()
