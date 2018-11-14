6684d228d5fca274ccb976b60d327765105c8b5a
gcccode <gcccode@gmail.com>
2018-11-14 19:41:05 +0800

    fix...

--- amazonwrapper.py
+++ amazonwrapper.py
@@ -988,7 +988,7 @@ def count_pending_asin(sqlmgr, top_tpye):
         for index in range(len(sale_task_array)):
             node = sale_task_array[index][0]
             node_table_name = node + '_' + top_tpye
-            condition = 'limited<>\'yes\' and seller<4 and price >= 12 and avg_sale>5'
+            condition = 'limited<>\'yes\' and seller<4 and price >= 12'
             pending_asin_array = get_all_data(sqlmgr.ad_sale_data, node_table_name, 'asin', condition)
             if pending_asin_array is False:
                 print("get all data in failure " + node_table_name, flush=True)
