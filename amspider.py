562be93ea298ff6c40a59d33d77404284dc63653
gcccode <gcccode@gmail.com>
2018-11-28 23:13:36 +0800

    fix...

--- amspider.py
+++ amspider.py
@@ -973,8 +973,8 @@ class AmazonSpider():
                             element = driver.find_element_by_xpath(tmp_symbol)
                             # print("Price is : " + element.text.strip('$ ').replace(',', ''), flush=True)
                             asin_info_data['price'] = getprice_uk(element.text)
-                            # if asin_info_data['price'] < 12:
-                            #     continue
+                            if asin_info_data['price'] < 12:
+                                continue
                         else:
                             continue
                     else:
@@ -991,8 +991,8 @@ class AmazonSpider():
                             element = driver.find_element_by_xpath(tmp_symbol)
                             # print("Price is : " + element.text.strip('$ ').replace(',', ''), flush=True)
                             asin_info_data['price'] = getprice_uk(element.text)
-                            # if int(asin_info_data['price']) < 12:
-                            #     continue
+                            if int(asin_info_data['price']) < 12:
+                                continue
                         else:
                             continue
 
