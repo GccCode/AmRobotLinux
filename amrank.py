80232155b55f437f43c6242b8d8a5a3c16ed644a
gcccode <gcccode@gmail.com>
2018-12-03 17:29:52 +0800

    fix...

--- amrank.py
+++ amrank.py
@@ -115,10 +115,8 @@ def is_keyword_rank_unavailable(sqlmgr, keyword, type):
     rank = amazonwrapper.get_all_data(sqlmgr.ad_rank_data, task_table, cur_date, status)
     if rank is not False:
         if rank[0][0] == '2050':
-            print(rank[0][0], flush=True)
             return True
         else:
-            print(rank[0][0], flush=True)
             return False
     else:
         return True
